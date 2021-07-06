1fab41d08b34f65bd782531cd1e97f60eb5a61ab
ko1596 <ko89022189@gmail.com>
2021-07-06 15:13:51 +0800

    動態畫圖waitKey

--- opcv/opcv.cpp
+++ opcv/opcv.cpp
@@ -1,9 +1,17 @@
 ﻿#include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <ctime> 
 
 using namespace std;
 using namespace cv;
+
+void   Delay(int   time)//time*1000為秒數 
+{
+	clock_t   now = clock();
+
+	while (clock() - now < time);
+}
 
 int main()
 {
@@ -15,8 +23,16 @@ int main()
 	// 画矩形
 	Rect r(250, 250, 250, 250);
 	//rectangle(img, r, Scalar(0, 255, 255), 3);
-	rectangle(img, r, Scalar(0, 0, 255), -1);
-	imshow("画板", img);
+	int i = 0;
+
+	while (true)
+	{
+		rectangle(img, r, Scalar(0, 0, i), -1);
+		if (i > 254) i = 0;
+		else i++;
+		imshow("画板", img);
+		waitKey(10);
+	}
 	waitKey();
 	return 0;
 }
