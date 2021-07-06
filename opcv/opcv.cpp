37397f8c3afd7b4d3e7b9e736e408f9950899357
ko1596 <ko89022189@gmail.com>
2021-07-06 14:45:42 +0800

    加入專案檔案。

---
+++ opcv/opcv.cpp
@@ -0,0 +1,18 @@
+﻿#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	namedWindow("a", 1);
+	moveWindow("a", 0, 0);
+	Mat pic = imread("pic.jpg", 1);
+	imshow("a", pic);
+	waitKey(0);
+	destroyAllWindows();
+	system("pause");
+	return 0;
+}
