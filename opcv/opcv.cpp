84e50c0843f899e72c4b6f0a31580c51977ac8a5
ko1596 <ko89022189@gmail.com>
2021-07-06 14:57:27 +0800

    畫正方形

--- opcv/opcv.cpp
+++ opcv/opcv.cpp
@@ -7,12 +7,16 @@ using namespace cv;
 
 int main()
 {
-	namedWindow("a", 1);
-	moveWindow("a", 0, 0);
-	Mat pic = imread("pic.jpg", 1);
-	imshow("a", pic);
-	waitKey(0);
-	destroyAllWindows();
-	system("pause");
+	// 设置窗口
+		Mat img = Mat::zeros(Size(800, 600), CV_8UC3);
+
+	img.setTo(255);              // 设置屏幕为白色
+	
+	// 画矩形
+	Rect r(250, 250, 250, 250);
+	//rectangle(img, r, Scalar(0, 255, 255), 3);
+	rectangle(img, r, Scalar(0, 0, 255), -1);
+	imshow("画板", img);
+	waitKey();
 	return 0;
 }
